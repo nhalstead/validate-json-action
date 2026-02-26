7182d9d43e3dcdd53b49c15a921e7b983a074a31
Noah Halstead <nhalstead@users.noreply.github.com>
2026-02-26 13:56:19 -0500

    fix tests to mock the github actions interface

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,1 +1,11 @@
 export const spyOnLog = jest.spyOn(global.console, 'log').mockImplementation(() => jest.fn());
+
+jest.mock('@actions/core', () => ({
+    info: jest.fn(),
+    error: jest.fn(),
+    debug: jest.fn(),
+    warning: jest.fn(),
+    setFailed: jest.fn(),
+    setOutput: jest.fn(),
+    getInput: jest.fn(),
+}));
